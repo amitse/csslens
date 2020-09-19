629db1e5f2ba0d0d551ab7b76cd3cc2f6e8e1d2d
Amit Sengupta <sengupta.amit@outlook.com>
2020-09-20 02:30:03 +0530

    Updates

---
+++ precache-manifest.b98ab5c45df7f7864e70b42c47652b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2598e41b360e63eed0e0e483b08aabef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c026c3b27afbd72627e",
+    "url": "/static/css/main.61f31393.chunk.css"
+  },
+  {
+    "revision": "4030653cdabeebca5bd4",
+    "url": "/static/js/2.8a26d4ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a26d4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c026c3b27afbd72627e",
+    "url": "/static/js/main.426e0ff3.chunk.js"
+  },
+  {
+    "revision": "0818fd35490369ab00f1",
+    "url": "/static/js/runtime-main.5ad694cd.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
